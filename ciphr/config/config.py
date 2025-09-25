8b2dff08a3504c9cef9897783a372ff2e9c08427
Pratik Jawahar <55033105+PRAkTIKal24@users.noreply.github.com>
2025-09-25 14:38:41 +0100

    Update LLM_QUESTIONS in config.py
    Replaced a question in LLM_QUESTIONS with a new one related to dark matter.

--- ciphr/config/config.py
+++ ciphr/config/config.py
@@ -9,7 +9,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 LLM_QUESTIONS = [
     "What is the main physics phenomenon studied by this paper",
-    "When was this paper published on arxiv?",
+    "Is this work related to dark matter searches? If yes, how?",
     "Does this paper present experimental results? If yes, what is the name of the experimental apparatus?",
 ]
 
